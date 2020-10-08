babf902392fc51ea724c14a7e45fe1e94bb610ec
Danil <danylo.kruhlikov@nure.ua>
2020-10-08 14:39:54 +0300

    add test

---
+++ precache-manifest.3190265240822e54e8545af2a93ec4c5.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93f4d33d3ff232674b01098e30aec60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2091989e6f604fce3b9c",
+    "url": "/static/css/3.eb1c6ebf.chunk.css"
+  },
+  {
+    "revision": "1cf546d4d5c41cb3b996",
+    "url": "/static/css/4.84ec0dbc.chunk.css"
+  },
+  {
+    "revision": "b1b488c54e207704b01f",
+    "url": "/static/css/5.cc525da6.chunk.css"
+  },
+  {
+    "revision": "65e67584ea4ba9b874e4",
+    "url": "/static/css/main.a4ac9b0a.chunk.css"
+  },
+  {
+    "revision": "723982930af5203bfb0b",
+    "url": "/static/js/0.339e2c2c.chunk.js"
+  },
+  {
+    "revision": "acb6fc12e54ce3f8f5dd",
+    "url": "/static/js/10.fb587301.chunk.js"
+  },
+  {
+    "revision": "d4a7f132c3413416427b",
+    "url": "/static/js/11.3dc6ced1.chunk.js"
+  },
+  {
+    "revision": "2091989e6f604fce3b9c",
+    "url": "/static/js/3.92dac2b3.chunk.js"
+  },
+  {
+    "revision": "715ad043f94d4942e49f6915ad603bae",
+    "url": "/static/js/3.92dac2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf546d4d5c41cb3b996",
+    "url": "/static/js/4.3cc4dd48.chunk.js"
+  },
+  {
+    "revision": "b1b488c54e207704b01f",
+    "url": "/static/js/5.76842073.chunk.js"
+  },
+  {
+    "revision": "b7f8dbc07d1e78229bb6",
+    "url": "/static/js/6.96afc3fc.chunk.js"
+  },
+  {
+    "revision": "39121c1e48a1b1fd0f52",
+    "url": "/static/js/7.ad58985f.chunk.js"
+  },
+  {
+    "revision": "ac6a44b5d8e6c145ab2f",
+    "url": "/static/js/8.acf1800a.chunk.js"
+  },
+  {
+    "revision": "deba982d4cc437020d48",
+    "url": "/static/js/9.311849ec.chunk.js"
+  },
+  {
+    "revision": "65e67584ea4ba9b874e4",
+    "url": "/static/js/main.f0e597ac.chunk.js"
+  },
+  {
+    "revision": "ab8c8a418ed6890b6890",
+    "url": "/static/js/runtime-main.a5636519.js"
+  },
+  {
+    "revision": "4b70bb1951603456b82657a9e2d63e98",
+    "url": "/static/media/anastasia.4b70bb19.png"
+  },
+  {
+    "revision": "d5c183349071521ae64d6aa46a397539",
+    "url": "/static/media/calendar.d5c18334.svg"
+  },
+  {
+    "revision": "9758d34985a1fc7fa63b200844f6f3a5",
+    "url": "/static/media/casinoroo.9758d349.png"
+  },
+  {
+    "revision": "43c36282fccee3968e66b590578f253f",
+    "url": "/static/media/charitybingo.43c36282.png"
+  },
+  {
+    "revision": "103a89649eee469a99f112f08061f5eb",
+    "url": "/static/media/circusbingo.103a8964.png"
+  },
+  {
+    "revision": "27cb2358542f621a83e6180cad02bc23",
+    "url": "/static/media/cloverbingo.27cb2358.png"
+  },
+  {
+    "revision": "d5d95524e6ccd81a9057903fd07cae06",
+    "url": "/static/media/diceden.d5d95524.png"
+  },
+  {
+    "revision": "e68b3f556e9a82e05ae84d338c9b70fb",
+    "url": "/static/media/flash.e68b3f55.svg"
+  },
+  {
+    "revision": "48c719bbd6c9d6ca89da685b09e58881",
+    "url": "/static/media/galyna.48c719bb.png"
+  },
+  {
+    "revision": "0d3932ed0cd1b1f0394efbf0b9f3f696",
+    "url": "/static/media/jackpotslot.0d3932ed.png"
+  },
+  {
+    "revision": "84781f7aa3b3cf616899050a9ef8931e",
+    "url": "/static/media/line-chart.84781f7a.svg"
+  },
+  {
+    "revision": "77b2bce228f6c1fa102e3fdc155c10fb",
+    "url": "/static/media/link.77b2bce2.svg"
+  },
+  {
+    "revision": "19eee23f6fe8e1e9f1c5249b7d43dfaf",
+    "url": "/static/media/m11.19eee23f.png"
+  },
+  {
+    "revision": "034231c35ee27998270f9c3e5f9bdbe1",
+    "url": "/static/media/m22.034231c3.png"
+  },
+  {
+    "revision": "6c6357b7b8170f1f2e018be40e51bc48",
+    "url": "/static/media/m33.6c6357b7.png"
+  },
+  {
+    "revision": "371e2ca52029b00d23a0a4cf58794ca2",
+    "url": "/static/media/miamijackpots.371e2ca5.png"
+  },
+  {
+    "revision": "5809ba2e26531908d0cf34472368e8f0",
+    "url": "/static/media/monitor.5809ba2e.png"
+  },
+  {
+    "revision": "7b5b3fc91aa26c9a4bfce65289c79a57",
+    "url": "/static/media/palacebingo.7b5b3fc9.png"
+  },
+  {
+    "revision": "e0a4d972d9f55f421c536c62262bc04f",
+    "url": "/static/media/picture.e0a4d972.svg"
+  },
+  {
+    "revision": "98c275f6473817403522fdd7089051fe",
+    "url": "/static/media/pixiebingo.98c275f6.png"
+  },
+  {
+    "revision": "0ff53ac967333618e3e59666efc8b9d7",
+    "url": "/static/media/qink.0ff53ac9.png"
+  },
+  {
+    "revision": "260c1f227529781fbd0e74467d3a632a",
+    "url": "/static/media/royalbets.260c1f22.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7a60649780d71d414f31caac424435b",
+    "url": "/static/media/slotsjungle.b7a60649.png"
+  },
+  {
+    "revision": "447fed17ac4445cc1392539d8da17358",
+    "url": "/static/media/slotster.447fed17.png"
+  },
+  {
+    "revision": "5befec0d4e807e7217c367b9739d5400",
+    "url": "/static/media/spinslots.5befec0d.png"
+  },
+  {
+    "revision": "d5c191186ceb5f2c3960656e02adc836",
+    "url": "/static/media/stats.d5c19118.svg"
+  },
+  {
+    "revision": "e976e2bf2a253765b0d31d0b71270dc9",
+    "url": "/static/media/team-bg.e976e2bf.png"
+  },
+  {
+    "revision": "8548e7419fc27ae3a6fa89fa01ce4ac3",
+    "url": "/static/media/vipbet.8548e741.png"
+  }
+]);
